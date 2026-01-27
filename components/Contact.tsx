9f8ad5096ecbf1a8c2acfec54bc18cbea331f321
Facundo Musumeci <musumecifacundo@gmail.com>
2026-01-28 00:29:22 +1200

    security: move web3forms access key to environment variables

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -23,7 +23,7 @@ const Contact: React.FC = () => {
           'Accept': 'application/json'
         },
         body: JSON.stringify({
-          access_key: "8b11f288-f901-4b24-a7f1-cd2091d60b75",
+          access_key: import.meta.env.VITE_WEB3FORMS_ACCESS_KEY,
           ...formData,
           from_name: "Esencia Animal Web",
           subject: `Nuevo mensaje de ${formData.nombre} - Esencia Animal`
